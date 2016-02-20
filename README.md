97dc54be57de6e1950009a9a3ae76d9e7522529f
Javier Fernandez Pastrana <jferpas@gmail.com>
2016-02-20 20:17:03 +0100

    Introduced AT COMMAND configuration

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # SiKRadioConfiguration
-Configuration of 3DR SiK Radio Telemetry sending AT COMMAND through serial port 
+Configuration of 3DR SiK Radio Telemetry sending AT COMMAND through serial port
+
+
+				Windows configuration
+
+1º Download lastest Python version.
+https://www.python.org/downloads/
+
+2º Install pySerial library.
+https://pypi.python.org/pypi/pyserial
+
+
+				Linux configuration
+
+sudo apt-get install python python-serial
+
+				Documentation
+pySerial API.
+http://pyserial.readthedocs.org/en/latest/pyserial_api.html#classes  
--- conf3DR.py
+++ conf3DR.py
@@ -4,12 +4,33 @@ import serial
 port_com = "COM1"
 baud = "57600"
 
-rf = serial.Serial(port_com,baud, timeout=5)
+rf = serial.Serial(port_com, 9600, EIGHTBITS, PARITY_NONE, STOPBITS_ONE, timeout=5, xonxoff=False, rtscts=False, write_timeout=None, dsrdtr=False, inter_byte_timeout=None)
 try:
 	time.sleep(0.5)
 	rf.write(b'+++')
 	time.sleep(3)
 	response = rf.readall()
-	print(response)	
+	print(response)
+	rf.write(b'RT\r\n')
+	time.sleep(1)
+	response = rf.readall()
+	print(response)
+	"""
+	RT&F
+	RTS1=19
+	RTS2=19
+	RTS3=20
+	RTS4=20
+	RTS5=1
+	RTS6=0
+	RTS8=414000
+	RTS9=427000
+	RTS10=50
+	RTS11=100
+	RT&W
+	RTZ
+	
+	
+	
 finally:
 	rf.close()
